547041808c67d27dade3b42474b73657658025fc
Pol <polegars@microsoft.com>
2022-12-15 10:47:47 +0000

    too far Polichan

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
@@ -9,16 +10,15 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
-
+    static int distance(int[] s, int[] b) {
+        return Math.abs(s[1] - b[1]) + Math.abs(s[0] - b[0]);
+    }
 
     static class Test {
         String t(int[] s) {
             return s[0] + ", " + s[1];
         }
 
-        int distance(int[] s, int[] b) {
-            return Math.abs(s[1] - b[1]) + Math.abs(s[0] - b[0]);
-        }
 
         int minX = Integer.MAX_VALUE;
         int maxX = Integer.MIN_VALUE;
@@ -37,7 +37,7 @@ public class Main {
                 maxY = b[1];
         }
 
-        void test() throws IOException {
+        void test(String arg) throws IOException, InterruptedException {
 
             HashMap<String, String> map = new HashMap<>();
 
@@ -60,8 +60,8 @@ public class Main {
 //                addToMax(b);
                 int distance = distance(s, b);
 
-                addToMax(new int[] { s[0] - distance, s[1] - distance});
-                addToMax(new int[] { s[0] + distance, s[1] + distance});
+                addToMax(new int[]{s[0] - distance, s[1] - distance});
+                addToMax(new int[]{s[0] + distance, s[1] + distance});
 
                 map.put(t(s), "S");
                 points.add(new int[][]{s, b});
@@ -78,49 +78,32 @@ public class Main {
 
             }
 
-            int count = 0;
-            for (int i = minX ; i < maxX; i++) {
-                boolean isCovered = false;
-                int[] curr = new int[]{i, 2000000};
-                for (int j = 0; j < points.size(); j++) {
-                    int[][] ints = points.get(j);
-                    int distance = distance(ints[0], ints[1]);
-                    if (distance(ints[0], curr) <= distance) {
-                        isCovered = true;
-                    }
-                    if (arrayToPoint(ints[1]).equals(arrayToPoint(curr))) {
-                        isCovered = false;
-                        break;
-                    }
-                }
-                if (isCovered) {
-                    System.out.print("#");
-                    count++;
-                } else {
-                    System.out.print(".");
-                }
-            }
-            System.out.println(count);
-//                for (int[] pari : map.keySet()) {
-//                    int[] point = stringToArray(key);
-//
-//                    if (map.get(key).equals("S")) {
-//                        if (distance(point, curr) )
-//                    }
-//                    String s = map.get(t());
-//                    if (s != null && !s.equals("B")) {
-//                        count++;
-//                    }
-//
-//                    if (s == null) {
-//                        s = ".";
-//                    }
-//                    System.out.print(s);
-//                }
-//            }
-//            System.out.println(count);
+//            int count = 0;
+            ArrayList<Worker> workers = new ArrayList<>();
 
+//            for (int i = 0; i < 10; i++) {
+            int i = Integer.parseInt(arg);
+            System.out.println(" starting line: " + i);
+                for (int j = 0; j < 10; j++) {
+                    Worker worker = new Worker();
+                    worker.x = i * (4000000 / 10);
+                    worker.y = j * (4000000 / 10);
+                    worker.size = 4000000 / 10;
+                    worker.points = points;
+                    System.out.println("Starting " + worker.x + "," + worker.y + "  " + worker.size);
+                    workers.add(worker);
+                    worker.start();
+                }
+//            }
+            for (Worker worker : workers) {
+                worker.join();
+
+                System.out.print("j");
+            }
+
+//            System.out.println(count);
         }
+
 
         int[] stringToArray(String s) {
             return Arrays.stream(s.split(","))
@@ -134,13 +117,65 @@ public class Main {
 
     }
 
+    static class Worker extends Thread {
+        int x, y, size;
+        ArrayList<int[][]> points;
 
-    public static void main(String[] args) throws IOException {
+
+        @Override
+        public void run() {
+            super.run();
+            extracted();
+            System.out.println("hello");
+        }
+
+        private void extracted() {
+            for (int k = y; k <= y + size; k++) { // y
+                if (x == 0 && y ==0){
+                    System.out.println(k);
+                }
+                for (int i = x; i <= x + size; i++) { //x
+
+                    boolean isCovered = false;
+                    int[] curr = new int[]{i, k};
+                    for (int j = 0; j < points.size(); j++) {
+                        int[][] ints = points.get(j);
+                        int distance = distance(ints[0], ints[1]);
+                        if (distance(ints[0], curr) <= distance) {
+                            isCovered = true;
+                        }
+//                        if (arrayToPoint(ints[1]).equals(arrayToPoint(curr))) {
+//                            isCovered = false;
+//                            break;
+//                        }
+                    }
+                    if (isCovered) {
+//                        System.out.print("#");
+//                        count++;
+                    } else {
+                        System.out.println(Arrays.toString(curr));
+                        System.out.println(curr[0] * 4000000 + curr[1]);
+                        System.out.print(".");
+                        return;
+                    }
+                }
+//                System.out.println(k);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 //        System.out.println("Hello world!");
 //        BufferedReader br = new BufferedReader(
 //                new InputStreamReader(new FileInputStream("day3")));
 //
-        new Test().test();
+
+        for (String arg : args) {
+
+            System.out.println(arg);
+        }
+        new Test().test(args[0]);
     }
 
 }
